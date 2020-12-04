a9af45a0cf4a7000ca89421f03ed1f710a02da5c
Daniel <aedaniel@gmail.com>
2020-12-04 16:39:18 -0600

    201204

--- 2020b/splanetas.py
+++ 2020b/splanetas.py
@@ -1,9 +1,6 @@
 import numpy as np
 
 def aceleraciones():
-    # Inicializar en cero todas las aceleraciones
-    a = np.zeros( (27) )
-
     # Calcular aceleración del planeta i
     for i in range (9) :
         indexi = 3 * i
@@ -37,9 +34,43 @@ def aceleraciones():
             a[indexk + 1] = a[indexk + 1] - M[i] * (yk - yi) * invrpk
             a[indexk + 2] = a[indexk + 2] - M[i] * (zk - zi) * invrpk
 
+def velocidades (v, a, dt):
+    return v + a * dt
+
+
+def posiciones (r, v, dt):
+    return r + v * dt 
+
 Ms = 39.428
-M = np.array ([6.547 × 10−6, 9.652 × 10−5, 1.185 × 10−4,......])   
-r = np.array ([0.387098,0,0,0.723332,0,0.... ]) 
-v = np.array ([0,0.723332,0, 0,7.38415,0,......])        
+M = np.array ([6.547*10.0**(-6), 9.652*10.0**(-5), 1.185*10.0**(-4),
+    1.272 *10.0**(-5), 3.764 *10.0**(-2),1.127*10.0**(-2),
+    1.721 *10.0**(-3), 2.030 *10.0**(-3),2.775 *10.0**(-7)])   
 
+#M = np.array([0.000006547, 0.00009652,0.0001185, 
+#              0.00001272, 0.03764, 0.01127, 
+#              0.001721, 0.002030, 0.0000002775])
 
+r = np.array([0.387098,0,0, 0.723332,0,0, 1.000000,0,0,
+    1.523689,0,0, 5.202758,0,0, 9.542824,0,0, 
+    19.19206,0,0, 30.06893,0,0, 39.4817,0,0])
+v = np.array([0.0,10.0934,0.0, 0.0,7.38415,0.0, 0,6.27978,0.0, 
+    0.0, 5.08792,0.0, 0,2.75311,0.0, 0,2.03257,0.0, 
+    0.0,1.43348,0.0, 0.0,1.14525,0.0, 0.0,0.99945, 0.0])   
+a = np.zeros( (27) )
+dt = 0.01
+
+x = []
+
+for n in range(5000):
+    # Inicializar en cero todas las aceleraciones
+    a = np.zeros( (27) )
+    aceleraciones()
+
+    v = velocidades(v,a,dt)
+
+    r = posiciones(r,v,dt)
+
+    x.append(r)
+
+#graficar x
+print(x)
